5fc4112faf6c4c558980ac5e86c9694a5299b136
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-01 10:59:20 +0200

    Added CollectionListTable

---
+++ src/Admin/ListTable/CollectionListTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace RebelCode\WordPress\Admin\ListTable;
+
+use Dhii\Collection\CollectionInterface;
+use RebelCode\WordPress\CollectionAwareInterface;
+use RebelCode\WordPress\HasCollectionTrait;
+
+/**
+ * A list table that displays items in a collection.
+ *
+ * @since [*next-version*]
+ */
+class CollectionListTable extends AbstractBaseListTable implements CollectionAwareInterface
+{
+    use HasCollectionTrait;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param CollectionInterface $collection The collection of items.
+     */
+    public function __construct(CollectionInterface $collection)
+    {
+        parent::__construct();
+
+        $this->_setCollection($collection);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function _getItems()
+    {
+        return $this->_getCollection()->getItems();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function getCollection()
+    {
+        return $this->_getCollection();
+    }
+}
